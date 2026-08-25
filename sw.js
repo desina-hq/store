2b4163bd5e078717da296f3ab1972a8d986c65ca
desina-hq <desina.com@gmail.com>
2026-08-25 05:41:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('desina-v10').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/index-ar.html',
+      '/logo-512.png'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
